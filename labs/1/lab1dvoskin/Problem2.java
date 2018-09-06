286c1dfb548e2ae1a20a76274a311b105a771a12
Phillip Dvoskin <38960968+PhillDvoskin@users.noreply.github.com>
2018-09-06 18:00:50 -0400

    Delete Problem2.java

--- labs/1/lab1dvoskin/Problem2.java
+++
@@ -1,9 +0,0 @@
-package lab1dvoskin;
-
-public class Problem2 {
-	public static void main(String[] args) {
-		System.out.println(4 * (1 - (1.0 / 3) + (1.0 / 5) - (1.0 / 7) + (1.0 / 9) - (1.0 / 11)));
-		System.out.println(4 * (1 - (1.0 / 3) + (1.0 / 5) - (1.0 / 7) + (1.0 / 9) - (1.0 / 11) + (1.0 / 13)));
-	}
-
-}
