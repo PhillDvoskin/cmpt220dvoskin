8e09b6f3a688681f3316c2b1248b216355c0e67f
Phillip Dvoskin <phillip.dvoskin1@marist.edu>
2018-09-06 22:41:38 -0400

    lab 1
    Problems 1- 4 of lab 1

--- labs/1/lab1dvoskin/Problem1.java
+++ labs/1/lab1dvoskin/Problem1.java
@@ -2,6 +2,9 @@ package lab1dvoskin;
 
 public class Problem1 {
 	public static void main(String[] args) {
+		/* Write out the numerator and put the whole operation in parentheses
+		 * Insert division sign and then write out the denominator also in parentheses 
+		 */
 		System.out.println((15.4 * 6.5 - 4.3 * 3) / (46.3 - 3.5));
 	}
 }
--- labs/1/lab1dvoskin/Problem4.java
+++ labs/1/lab1dvoskin/Problem4.java
@@ -1,19 +1,20 @@
 package lab1dvoskin;
-
+// Import a Scanner
 import java.util.Scanner;
 
 public class Problem4 {
 	public static void main(String[] args) {
-		
+		// Allow for user to input later in the program
 		Scanner input = new Scanner(System.in);
-		
+		// Print out the statement below
 		System.out.print("Enter a value for m: ");
+		// Ask the user to input an integer value for m
 		int m = input.nextInt();
-		
+		// Define the given value for c
 		int c = 299972458;
-		
+		// Define the double energy by using m multiplied by c squared
 		double energy = m * Math.pow(c, 2);
-		
+		// Print out the result of energy
 		System.out.println("E is " + energy);
 		
 		
