6e5dc3a0313b0621e9e55b7decd7d6800f1988a7
Phillip Dvoskin <phillip.dvoskin1@marist.edu>
2018-11-16 21:44:48 -0500

    lab 10 files
    All the parts of lab10 that I could complete

---
+++ labs/10/lab10dvoskin/Rectangle.java
@@ -0,0 +1,13 @@
+package lab10dvoskin;
+
+public class Rectangle {
+	int length;
+	int width;
+	
+	public Rectangle(int i, int j) {
+		this.width = width;
+		this.length = length;
+		
+		System.out.println("The rectangle has a width " + width + " and length " + length);
+	}
+}
